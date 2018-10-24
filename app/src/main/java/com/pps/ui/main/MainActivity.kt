d681b1d56ed6dab5a78f1363929684f0c390f68f
Joel Dean <jdeanjj1000@gmail.com>
2018-10-23 22:57:13 -0500

    initial commit

---
+++ app/src/main/java/com/pps/ui/main/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.pps.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.pps.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
